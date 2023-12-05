d86db2f2c3aee6f1e3e45211c19b2f407e6305c1
LuisFernandoL <siul.l@hotmail.com>
2023-12-05 12:13:34 -0300

    first commit

---
+++ songs/views.py
@@ -0,0 +1,17 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Song
+from rest_framework.pagination import PageNumberPagination
+from .serializers import SongSerializer
+from rest_framework.generics import ListCreateAPIView
+
+
+class SongView(ListCreateAPIView, PageNumberPagination):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(album_id=self.kwargs.get("pk"))
---
+++ users/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with that username already exists.",
+            )
+        ],
+    )
+    email = serializers.EmailField(
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    password = serializers.CharField(write_only=True)
+    full_name = serializers.CharField(max_length=50, required=False)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password", "full_name", "artistic_name"]
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+            if key == "password":
+                instance.set_password(value)
+
+        instance.save()
+
+        return instance
